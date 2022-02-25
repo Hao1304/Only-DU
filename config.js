d570b2df3186f1e70a432a167dadbb9318a82711
Kdragonone <90735607+Kdragonone@users.noreply.github.com>
2022-02-25 17:45:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: '',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay chơi game với mình nha!.',
     btnAccept: 'Okiiiii lun <3',
