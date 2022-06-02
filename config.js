2826f12512df81ac6946676503cc69b7b6fda3b9
buibaoduc <106752785+buibaoduc@users.noreply.github.com>
2022-06-02 21:14:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Dành cho Tôm cinh x1000",
+    introTitle: 'Chính em, người đang nhìn màn hình đấy!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^HaHa^^',
+    title: 'Do u know which side of my heart ? Its beside you !!!!',
+    desc: 'Muốn em là của riêng mình, chết trong vòng tay duyên tình , nếu mà thoát ra sẽ phải cưới cún',
+   
+    btnYes: 'Yêu lắm,yêu như nào ? <3',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Chiều mai 2h, cún đón đi chơi ha! ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.messenger.com/t/100066752500494/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
