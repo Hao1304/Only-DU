cc1955b0fe61fe80119d00f527abf0a27a4621d9
Hao1304 <106806330+Hao1304@users.noreply.github.com>
2022-06-03 16:22:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "You are my sunshine",
-    introTitle: 'Hà ơiiii',
+    introTitle: 'Bé ơiiii',
     introDesc: `I can’t really find the words to explain the way I feel
     when I hear your voice or when I see your face…
     all I can say is that I like the feeling.`,
     btnIntro: '<3',
-    title: ' When I first saw you I looked for a signature,because every masterpiece has one',
-    desc: 'Khi lần đầu nhìn thấy em, anh cố tìm một chữ ký, bởi vì mọi kiệt tác đều có chữ ký riêng. ',
-    btnYes: 'Thích anh cực kì :))',
-    btnNo: 'Không thích tý nào :(',
+    title: ' you are the sweetest thing i've ever known',
+    desc: 'bạn là điều ngọt ngào nhất mà tôi từng biết ',
+    btnYes: 'anh muốn gặp em sớm hơn',
+    btnNo: 'chê nha :(',
     question: 'Đố em biết I trong tiếng anh với ai trong tiếng thái mình giống nhau ở điểm nào đó?',
     btnReply: 'Trả lời anh <3',
     reply: 'Cả 2 đều là anh, và đều muốn em bên cạnh😘 ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Em có đồng ý ở cạnh anh không ?',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/tran.nghiasimple' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'lụm nha : )))',
+    messLink: 'https://www.facebook.com/messages/t/100043318952126' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
