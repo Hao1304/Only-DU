695b8714afe6179e807bd4dcbd7dceb1e8b547c1
Hao1304 <106806330+Hao1304@users.noreply.github.com>
2022-06-03 17:32:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    titleWeb: "Sent to Nhu Y",
-    introTitle: 'Nhu Y',
+    titleWeb: "Sent to Duy Uyen",
+    introTitle: 'DuUyen',
     introDesc: `falling for you 
                 nobody just you`,
     btnIntro: 'Hehehe',
-    title: 'Rảnh quá nên làm cho zui hoi à 🤓',
-    desc: 'chơi chung cho zui cả 2 ',
+    title: 'thêm tí muối cho cuộc sống mặn mà 🤓',
+    desc: 'xem những điều thú vị nhé bae ',
     btnYes: 'yêu em 🙆',
     btnNo: 'hok iu (nhma có bấm cũng hok được =))) ) ',
-    question: 'Lý do gì bạn see tình mình dzữ 🤔 ',
-    btnReply: 'sent reason to TH',
-    reply: 'Hok biết',
-    mess: 'Hỏi dzị thôi chứ mình cũng see tình bạn hok có lý do 🤓 ',
+    question: 'Lý do gì em không chia sẽ tâm tư cho anh biết 🤔 ',
+    btnReply: 'sent reason to Uyen',
+    reply: 'Đ' biết',
+    mess: 'Hỏi dzị thôi , anh hiểu em là được  🤓 ',
     messDesc: 'Mốt gặp dẫn mình đi ăn nhe',
-    btnAccept: 'Chỉ có ô cê chứ kh có câu trả lời khác',
-    messLink: 'https://www.facebook.com/kimmhng/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'lụm liền : ))',
+    messLink: 'https://www.facebook.com/quochao.nguyen.96558/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
