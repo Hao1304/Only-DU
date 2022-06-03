b6c6e8a5e293c4f39e4302ddde0e6d2672d2ac2e
Hao1304 <106806330+Hao1304@users.noreply.github.com>
2022-06-03 21:42:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi đến người anh yêu",
-    introTitle: 'Chu thị minh Hạnh',
+    introTitle: 'Trần Vũ Duy Uyên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim anh 
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnIntro: '^^Only U❤️^^',
+    title: 'rất muốn gặp anh rồi phải khum 🥰',
+    desc: 'Phải chăng em đã say ngay từ lần quẹt nhầm đầu tiên : )) ',
+    btnYes: 'rất muôn nha <33',
+    btnNo: 'Chê nha anh trai :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'vì em thích anh nhiều lắm luôn ý  ',
-    mess: 'Mình biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Làm người yêu anh nhé,bbi!.',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì anh là người em tìm kím',
+    mess: 'Anh biết mà :. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chờ anh nhé , vài ngày nữa thôi!.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/kiet.ntz.5/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/quochao.nguyen.96558/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
